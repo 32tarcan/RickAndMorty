2ef2aa7c451a4d89b59e953ed66b2668cf413114
32tarcan <bahadir.tarcan41@gmail.com>
2023-01-05 22:12:50 +0300

    Initial commit.

--- RickAndMorty/Resources/SceneDelegate.swift
+++ RickAndMorty/Resources/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
       
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
-        let vc = ViewController()
+        let vc = RMTabBarController()
         
         let window = UIWindow(windowScene: windowScene)
         window.rootViewController = vc

8b755be56f0e3acd57a177ccb6bafcddc4353a9b
32tarcan <bahadir.tarcan41@gmail.com>
2023-01-05 22:18:15 +0300

    NavigationController created.

--- RickAndMorty/Controllers/Core/RMTabBarController.swift
+++ RickAndMorty/Controllers/Core/RMTabBarController.swift
@@ -13,6 +13,22 @@ class RMTabBarController: UITabBarController {
         super.viewDidLoad()
         view.backgroundColor = .red
     }
+    
+    private func setUpTabs() {
+        let charactersVC = RMCharacterViewController()
+        let locationsVC = RMLocationViewController()
+        let episodesVC = RMEpisodeViewController()
+        let settingsVC = RMSettingsViewController()
+        
+        let nav1 = UINavigationController(rootViewController: charactersVC)
+        let nav2 = UINavigationController(rootViewController: locationsVC)
+        let nav3 = UINavigationController(rootViewController: episodesVC)
+        let nav4 = UINavigationController(rootViewController: settingsVC)
+        
+        setViewControllers([nav1, nav2, nav3, nav4],
+                           animated: true
+        )
+    }
 
 
 }

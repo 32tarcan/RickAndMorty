0e1f20ed0ac552c01151ccd14d419019fd80c38f
32tarcan <bahadir.tarcan41@gmail.com>
2023-01-05 22:12:33 +0300

    Models created.

---
+++ RickAndMorty/Models/RMEpisode.swift
@@ -0,0 +1,8 @@
+//
+//  RMEpisode.swift
+//  RickAndMorty
+//
+//  Created by Bahadır Tarcan on 5.01.2023.
+//
+
+import Foundation
---
+++ RickAndMorty/Models/RMLocation.swift
@@ -0,0 +1,8 @@
+//
+//  RMLocation.swift
+//  RickAndMorty
+//
+//  Created by Bahadır Tarcan on 5.01.2023.
+//
+
+import Foundation

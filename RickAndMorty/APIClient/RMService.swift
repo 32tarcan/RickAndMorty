bb9cfe2cf5223fa2e0ad16a3fb04604f06ddb0b0
32tarcan <bahadir.tarcan41@gmail.com>
2023-01-11 15:48:47 +0300

    Result and Error added.

--- RickAndMorty/APIClient/RMService.swift
+++ RickAndMorty/APIClient/RMService.swift
@@ -18,7 +18,8 @@ final class RMService {
     /// - Parameters:
     ///  - request: Request instance
     ///  - completion: Callback with data or error
-    public func execute(_ request: RMRequest, completion: @escaping () -> Void) {
+    public func execute(_ request: RMRequest,
+                        completion: @escaping (Result<String, Error>) -> Void) {
         
     }
     

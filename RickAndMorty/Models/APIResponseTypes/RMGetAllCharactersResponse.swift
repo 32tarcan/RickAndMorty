7592557f795fa682de16a350c7968b502697f477
32tarcan <bahadir.tarcan41@gmail.com>
2023-01-14 11:05:40 +0300

    Info Models added.

--- RickAndMorty/Models/APIResponseTypes/RMGetAllCharactersResponse.swift
+++ RickAndMorty/Models/APIResponseTypes/RMGetAllCharactersResponse.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+struct RMGetAllCharactersResponse: Codable {
+    struct Info: Codable {
+        let count: Int
+        let pages: Int
+        let next: String?
+        let prev: String?
+        
+    }
+    
+    let info: Info
+    let results: [RMCharacter]
+    
+}
+
+
+
+
+
+
+

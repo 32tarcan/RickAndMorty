c5ba2944ec084544b45a3a1fc4dbaf430c92daaf
32tarcan <bahadir.tarcan41@gmail.com>
2023-01-14 10:28:49 +0300

    API Call added.

--- RickAndMorty/APIClient/RMService.swift
+++ RickAndMorty/APIClient/RMService.swift
@@ -14,6 +14,11 @@ final class RMService {
     /// Privalitezed constructor
     private init() {}
     
+    enum RMServiceError: Error {
+        case failedToCreateRequest
+        case failedToGetData
+    }
+    
     /// Send Rick and Morty API Call
     /// - Parameters:
     ///  - request: Request instance
@@ -22,9 +27,42 @@ final class RMService {
     public func execute<T:Codable>
     (_ request: RMRequest,
      expecting type: T.Type,
-      completion: @escaping (Result<T, Error>) -> Void) {
+      completion: @escaping (Result<T, Error>) -> Void)
+    {
+       guard let urlRequest = self.request(from: request) else {
+           completion(.failure(RMServiceError.failedToCreateRequest))
+            return
+        }
         
-        
+        let task = URLSession.shared.dataTask(with: urlRequest) { data, _, error in
+            guard let data = data, error == nil else {
+                completion(.failure(error ?? RMServiceError.failedToGetData))
+                return
+            }
+            // Decode response
+            do {
+                let json = try JSONSerialization.jsonObject(with: data)
+                print(String(describing: json))
+                //
+            }
+            catch {
+                completion(.failure(error))
+            }
+            
+        }
+        task.resume()
+    }
+    
+    //MARK: - Private
+    
+    private func request(from rmRequest: RMRequest) -> URLRequest? {
+        guard let url = rmRequest.url else {
+            return nil
+            
+        }
+        var request = URLRequest(url: url)
+        request.httpMethod = rmRequest.httpMethod
+        return request
     }
     
     

2dff113633c84032554e1b46fbe61c7d1e2c55ae
32tarcan <bahadir.tarcan41@gmail.com>
2023-03-09 22:04:44 +0300

    SwiftUI Settings View UI added.

--- RickAndMorty/Controllers/Core/RMSettingsViewController.swift
+++ RickAndMorty/Controllers/Core/RMSettingsViewController.swift
@@ -5,25 +5,47 @@
 //  Created by Bahadır Tarcan on 5.01.2023.
 //
 
+import SwiftUI
 import UIKit
 
 /// Controller to show various app options and settings
 class RMSettingsViewController: UIViewController {
     
-    private let viewModel = RMSettingsViewViewModel(
-        cellViewModels: RMSettingsOption.allCases.compactMap({
+    
+    private let settingsSwiftUIController = UIHostingController(
+        rootView: RMSettingsView(
+            viewModel: RMSettingsViewViewModel(
+            cellViewModels: RMSettingsOption.allCases.compactMap({
             return RMSettingsCellViewModel(type: $0)
             
         })
-    )
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .systemBackground
-        title = "Settings"
-    }
-    
-
+  )
+   )
+  )
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            view.backgroundColor = .systemBackground
+            title = "Settings"
+            addSwiftUIController()
+        }
    
-
-}
+    private func addSwiftUIController() { 
+        addChild(settingsSwiftUIController)
+        settingsSwiftUIController.didMove(toParent: self)
+        
+        view.addSubview(settingsSwiftUIController.view)
+        settingsSwiftUIController.view.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            settingsSwiftUIController.view.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            settingsSwiftUIController.view.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            settingsSwiftUIController.view.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+            settingsSwiftUIController.view.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        
+        
+        ])
+    }
+        
+        
+        }
---
+++ RickAndMorty/Views/SwiftUI/RMSettingsView.swift
@@ -0,0 +1,48 @@
+//
+//  RMSettingsView.swift
+//  RickAndMorty
+//
+//  Created by Bahadır Tarcan on 9.03.2023.
+//
+
+import SwiftUI
+
+struct RMSettingsView: View {
+    let viewModel: RMSettingsViewViewModel
+    
+    init(viewModel: RMSettingsViewViewModel) {
+        self.viewModel = viewModel
+    }
+    
+    
+    var body: some View {
+        List(viewModel.cellViewModels) { viewModel in
+            HStack {
+                if let image = viewModel.image {
+                    Image(uiImage: image)
+                        .resizable()
+                        .renderingMode(.template)
+                        .foregroundColor(Color.white)
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 20, height: 20)
+                        .foregroundColor(Color.red)
+                        .padding(8)
+                        .background(Color(viewModel.iconContainerColor))
+                        .cornerRadius(6)
+                }
+                Text(viewModel.title)
+                    .padding(.leading, 10)
+            }
+            .padding(.bottom, 3)
+        }
+    }
+}
+
+struct RMSettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        RMSettingsView(viewModel: .init(cellViewModels: RMSettingsOption.allCases.compactMap({
+            return RMSettingsCellViewModel(type: $0)
+            
+        })))
+    }
+}

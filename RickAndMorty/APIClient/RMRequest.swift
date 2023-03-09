276de78ff4dcff4268f57295b9cfe02ec66e4b55
32tarcan <bahadir.tarcan41@gmail.com>
2023-03-10 01:32:44 +0300

    Start Building Out Locations Tab

--- RickAndMorty/APIClient/RMRequest.swift
+++ RickAndMorty/APIClient/RMRequest.swift
@@ -131,6 +131,7 @@ final class RMRequest {
 extension RMRequest {
     static let listCharactersRequests = RMRequest(endpoint: .character)
     static let listEpisodesRequests = RMRequest(endpoint: .episode)
+    static let listLocationsRequests = RMRequest(endpoint: .location)
 }
 
 

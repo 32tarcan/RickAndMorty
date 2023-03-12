e007e426f32cd3d1f9790716a8d31afcc113d705
32tarcan <bahadir.tarcan41@gmail.com>
2023-03-13 01:18:34 +0300

    Added No Search Results View.

--- RickAndMorty/ViewModels/Search/RMNoSearchResultsViewViewModel.swift
+++ RickAndMorty/ViewModels/Search/RMNoSearchResultsViewViewModel.swift
@@ -5,8 +5,9 @@
 //  Created by Bahadır Tarcan on 13.03.2023.
 //
 
-import Foundation
+import UIKit
 
 struct RMNoSearchResultsViewViewModel {
-    
+    let title = "No Result"
+    let image = UIImage(systemName: "magnifyingglass.circle" )
 }
--- RickAndMorty/Views/Search/RMSearchView.swift
+++ RickAndMorty/Views/Search/RMSearchView.swift
@@ -15,7 +15,7 @@ final class RMSearchView: UIView {
     
     // SearchInputView(bar, selection buttons)
     
-    // No results view
+    private let noResultsView = RMNoSearchResultsView()
     
     // Results collectionView
     
@@ -23,14 +23,29 @@ final class RMSearchView: UIView {
     
      init(frame: CGRect, viewModel: RMSearchViewViewModel) {
          self.viewModel = viewModel
-        super.init(frame: frame)
-        backgroundColor = .red
-        translatesAutoresizingMaskIntoConstraints = false
+         super.init(frame: frame)
+         backgroundColor = .systemBackground
+         translatesAutoresizingMaskIntoConstraints = false
+         addSubviews(noResultsView)
+         addConstraints()
     }
 
     required init?(coder: NSCoder) {
         fatalError()
     }
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            noResultsView.widthAnchor.constraint(equalToConstant: 150),
+            noResultsView.heightAnchor.constraint(equalToConstant: 150),
+            noResultsView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            noResultsView.centerYAnchor.constraint(equalTo: centerYAnchor),
+        
+        
+        ])
+        
+    }
+    
 
 }
 

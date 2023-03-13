32bd903365591df1bfbd8c2c7a14f227c94c6192
32tarcan <bahadir.tarcan41@gmail.com>
2023-03-13 15:36:52 +0300

    Search Bar added.

--- RickAndMorty/Views/Search/RMSearchInputView.swift
+++ RickAndMorty/Views/Search/RMSearchInputView.swift
@@ -8,7 +8,66 @@
 import UIKit
 
 final class RMSearchInputView: UIView {
+    
+    private let searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.placeholder = "Search"
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        return searchBar
+        
+    }()
+    
+    private var viewModel: RMSearchInputViewViewModel? {
+        didSet {
+            guard let viewModel = viewModel, viewModel.hasDynamicOptions else {
+                return
+            }
+            let options = viewModel.options
+            createOptionSelectionViews(options: options)
+        }
+    }
+    
+    
+    //MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        translatesAutoresizingMaskIntoConstraints = false
+        backgroundColor = .systemPink
+        addSubviews(searchBar)
+        addConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            searchBar.topAnchor.constraint(equalTo: topAnchor),
+            searchBar.leftAnchor.constraint(equalTo: leftAnchor),
+            searchBar.rightAnchor.constraint(equalTo: rightAnchor),
+            searchBar.heightAnchor.constraint(equalToConstant: 58),
 
+        
+        
+        ])
+        
+    }
+    
+    private func createOptionSelectionViews(options: [RMSearchInputViewViewModel.DynamicOption]) {
+        for option in options {
+            print(option.rawValue)
+            
+        }
+        
+    }
+
+    public func configure(with viewModel: RMSearchInputViewViewModel) {
+        searchBar.placeholder = viewModel.searchPlaceholderText
+        self.viewModel = viewModel
+        
+    }
     
 
 }
--- RickAndMorty/Views/Search/RMSearchView.swift
+++ RickAndMorty/Views/Search/RMSearchView.swift
@@ -14,6 +14,7 @@ final class RMSearchView: UIView {
     // MARK: - Subviews
     
     // SearchInputView(bar, selection buttons)
+    private let searchInputView = RMSearchInputView()
     
     private let noResultsView = RMNoSearchResultsView()
     
@@ -26,8 +27,10 @@ final class RMSearchView: UIView {
          super.init(frame: frame)
          backgroundColor = .systemBackground
          translatesAutoresizingMaskIntoConstraints = false
-         addSubviews(noResultsView)
+         addSubviews(noResultsView, searchInputView)
          addConstraints()
+         
+         searchInputView.configure(with: RMSearchInputViewViewModel(type: viewModel.config.type))
     }
 
     required init?(coder: NSCoder) {
@@ -36,6 +39,14 @@ final class RMSearchView: UIView {
     
     private func addConstraints() {
         NSLayoutConstraint.activate([
+            // Search input view
+            searchInputView.topAnchor.constraint(equalTo: topAnchor),
+            searchInputView.leftAnchor.constraint(equalTo: leftAnchor),
+            searchInputView.rightAnchor.constraint(equalTo: rightAnchor),
+            searchInputView.heightAnchor.constraint(equalToConstant: 110),
+
+            
+            // No results.
             noResultsView.widthAnchor.constraint(equalToConstant: 150),
             noResultsView.heightAnchor.constraint(equalToConstant: 150),
             noResultsView.centerXAnchor.constraint(equalTo: centerXAnchor),

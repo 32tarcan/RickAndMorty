82ed175c985d2aab3bd26f1f7a29c2ac119412a1
32tarcan <bahadir.tarcan41@gmail.com>
2023-03-13 17:10:55 +0300

    Search Option Bottom Sheet added.

--- RickAndMorty/Controllers/Other/RMSearchViewController.swift
+++ RickAndMorty/Controllers/Other/RMSearchViewController.swift
@@ -90,6 +90,11 @@ final class RMSearchViewController: UIViewController {
 
 extension RMSearchViewController: RMSearchViewDelegate {
     func rmSearchView(_ searchView: RMSearchView, didSelectOption option: RMSearchInputViewViewModel.DynamicOption) {
-        print("Should present option picker")
+        let vc = RMSearchOptionPickerViewController(option: option) { selection in
+            print("Did select \(selection)")
+        }
+        vc.sheetPresentationController?.detents = [.medium()]
+        vc.sheetPresentationController?.prefersGrabberVisible = true
+        present(vc, animated: true)
     }
 }
